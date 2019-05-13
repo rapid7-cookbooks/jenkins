9dbcfc20c0c1c283f618c29f59cba539bfd34728
Eric Romani <39929885+eromani-r7@users.noreply.github.com>
2019-05-13 15:05:42 -0700

    Update node.rb

--- providers/node.rb
+++ providers/node.rb
@@ -19,6 +19,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+require "../resources/node"
 
 def load_current_resource
   @current_resource = Chef::Resource::JenkinsNode.new(@new_resource.node_name)
